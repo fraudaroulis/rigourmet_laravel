8e63c21b2f782cec4929629c760c858cabc0b1b3
George Pilpilidis <fraudaroulis@gmail.com>
2015-07-16 01:22:48 +0300

    Add return user data for testing

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -103,7 +103,7 @@
     @section('footer')
 <div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="false"></div>
 
-{{ $user }}
+{{ $user}} {{$test}}
 
         <!-- footer 2 -->
         <div id="footer2">
